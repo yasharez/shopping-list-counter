9b7731f626dd251edcf7d1fe91d15072f8c16b52
Yashar Zafari <yasharez@gmail.com>
2023-02-11 14:19:42 -0800

    Server and client connection working

--- shopping-list-client.py
+++ shopping-list-client.py
@@ -7,3 +7,15 @@
 from socket import *
 import json
 
+# Setup server info
+serverName, serverPort = '127.0.0.1', 5005
+
+# Create and bind socket for client
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+# Send message to server
+print('Sending-------------------------------------------------')
+req = 10000000 * 'a'
+clientSocket.sendall(req.encode())
+clientSocket.close()
